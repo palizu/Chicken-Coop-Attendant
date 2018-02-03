64a509734c0188407ca76a8f4536fe1b8c57e2b2
KedielM <km3556@drexel.edu>
2018-02-03 11:07:26 -0500

    Created TableViewController Swift files.

---
+++ Chicken Coop Attendant/Chicken Coop Attendant/CoopTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CoopTableViewCell.swift
+//  Chicken Coop Attendant
+//
+//  Created by Kediel on 2/3/18.
+//  Copyright © 2018 riOS. All rights reserved.
+//
+
+import UIKit
+
+class CoopTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Chicken Coop Attendant/Chicken Coop Attendant/SolarTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SolarTableViewCell.swift
+//  Chicken Coop Attendant
+//
+//  Created by Kediel on 2/3/18.
+//  Copyright © 2018 riOS. All rights reserved.
+//
+
+import UIKit
+
+class SolarTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
